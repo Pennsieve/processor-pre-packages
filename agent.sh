7110a98714a8e12f7939e6d8fb9ea68eafff0ac3
Kevin Wang <wankevin@sas.upenn.edu>
2024-11-18 14:51:38 -0500

    added more agent changes

--- agent.sh
+++ agent.sh
@@ -3,14 +3,12 @@
 # Currently: Looping through presigned URLs w/ wget to get presigned urls and download them - we want to login to the agent and USE agent to get presigned urls
 # Currently, we LOOP through and thus download files in a flat structure -- with map, we can retain nested folder structures
 # Adding spaces creates a lot of %20 in the filename when the file downloads
-pwd
 pennsieve agent
 pennsieve whoami
 
-# pennsieve dataset use $1
-# pennsieve download dataset $1 "/service/data"
-# pennsieve version
+pennsieve version
 pennsieve map $1 "~"
+pennsieve download dataset $1 "~"
 # pennsieve pull $2
 # pennsieve manifest sync $3
 
